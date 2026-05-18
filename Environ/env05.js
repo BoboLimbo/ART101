b9f842e140af4199cf2780fb62d994133adbad7c
BoboLimbo <johnnychung333@gmail.com>
2026-05-17 23:48:11 -0700

    beautifulpush

---
+++ Environ/env05.js
@@ -0,0 +1,81 @@
+let appearingImage1 = document.getElementById("hey");
+let appearingImage2 = document.getElementById("bubble-hollander");
+let buttonImage = document.getElementById("weather-button");
+
+let environmentTitle = "Up in the Sky";
+
+let environmentElements = ["clouds", "warm sky", "sunset", "flying people"];
+
+let mainEntity = {
+    name: "Plane Hollander",
+    mood: "confused",
+    isMoving: true,
+    favoriteElement: environmentElements[1]
+};
+
+$("#super").hover(
+    function() {
+        $(this).addClass("perked");
+    },
+    function() {
+        $(this).removeClass("perked");
+    }
+);
+
+$("#hollander img").hover(
+    function() {
+        $(this).addClass("perked");
+    },
+    function() {
+        $(this).removeClass("perked");
+    }
+);
+
+$("#weather-button").hover(
+    function() {
+        $(this).addClass("perked");
+    },
+    function() {
+        $(this).removeClass("perked");
+    }
+);
+
+$("#super").click(function () {
+
+    console.log("prime is clicked");
+    appearingImage1.src = "bubble-prime.png";
+    appearingImage1.style.width = "140px";
+    appearingImage1.style.height = "auto";
+
+
+})
+
+
+$("#hollander img").click(function () {
+
+    console.log("hollander is clicked");
+    appearingImage2.style.display = "block";
+
+})
+
+buttonImage.addEventListener("click", function() {
+
+    let userInput = prompt("Prime here, how about a change of scenery? You want noon, sunset, or night?");
+    let keyword = userInput ? userInput.toLowerCase() : "";
+
+    if (keyword === "noon") {
+        document.body.style.backgroundImage = "url('Noon.jpeg')";
+
+    } else if (keyword === "sunset") {
+        document.body.style.backgroundImage = "url('Sunset.jpg')";
+
+    } else if (keyword === "night") {
+        document.body.style.backgroundImage = "url('Night.jpeg')";
+
+    } else {
+        alert("Yeah... I got no idea what that means. Try 'noon', 'sunset', or 'night'.");
+    }
+
+});
+
+
